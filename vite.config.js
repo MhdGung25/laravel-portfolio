9747b15b6034f447cc127c5e3b73e70fac1aa712
Muhammad Agung <agung@example.com>
2025-11-06 13:51:27 +0700

    Configure Vite for Netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    build: {
+        outDir: 'dist',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-});
+})
